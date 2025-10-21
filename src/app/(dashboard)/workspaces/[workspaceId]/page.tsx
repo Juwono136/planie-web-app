40fc9ea11e32ea254fc509c2201ebf4990e10cdc
unosega@gmail.com <unosega@gmail.com>
2025-10-21 23:14:37 +0700

    add workspaces and members features
    Signed-off-by: unosega@gmail.com <unosega@gmail.com>

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -0,0 +1,9 @@
+const WorkspaceIdPage = () => {
+  return (
+    <div>
+      <h1>Workspace Id</h1>
+    </div>
+  );
+};
+
+export default WorkspaceIdPage;
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,14 +2,37 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
-import { ID } from "node-appwrite";
+import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberRoles } from "@/features/members/types";
+import { generateInviteCode } from "@/lib/utils";
 
-const app = new Hono().post(
-  "/",
-  zValidator("form", createWorkspaceSchema),
-  sessionMiddleware,
-  async (c) => {
+const app = new Hono()
+  .get("/", sessionMiddleware, async (c) => {
+    const user = c.get("user");
+    const databases = c.get("databases");
+
+    const members = await databases.listDocuments({
+      databaseId: DATABASE_ID,
+      collectionId: MEMBERS_ID,
+      queries: [Query.equal("userId", user.$id)],
+    });
+
+    if (members.total === 0) {
+      return c.json({ data: { documents: [], total: 0 } });
+    }
+
+    const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+    const workspaces = await databases.listDocuments({
+      databaseId: DATABASE_ID,
+      collectionId: WORKSPACES_ID,
+      queries: [Query.orderDesc("$createdAt"), Query.contains("$id", workspaceIds)],
+    });
+
+    return c.json({ data: workspaces });
+  })
+  .post("/", zValidator("form", createWorkspaceSchema), sessionMiddleware, async (c) => {
     const databases = c.get("databases");
     const storage = c.get("storage");
     const user = c.get("user");
@@ -41,11 +64,22 @@ const app = new Hono().post(
         name,
         userId: user.$id,
         imageUrl: uploadImageUrl,
+        inviteCode: generateInviteCode(6),
+      },
+    });
+
+    await databases.createDocument({
+      databaseId: DATABASE_ID,
+      collectionId: MEMBERS_ID,
+      documentId: ID.unique(),
+      data: {
+        userId: user.$id,
+        workspaceId: workspace.$id,
+        role: MemberRoles.ADMIN,
       },
     });
 
     return c.json({ data: workspace });
-  }
-);
+  });
 
 export default app;
